64c5832a5c1cb899d234b4ff04074a85e9465bf8
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-06-14 18:32:07 -0300

    feat:req 2 done

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('txt'):
+            return sys.stderr.write("Formato inválido\n")
+        with open(path_file, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return
