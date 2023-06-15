b2416a91c0ad0bf800e45249a147748a7f41937d
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-06-15 17:12:34 -0300

    feat:req 3 done

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    object = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file),
+        'linhas_do_arquivo': file,
+    }
+
+    for item in instance.queue:
+        if item['nome_do_arquivo'] == object['nome_do_arquivo']:
+            return False
+
+    instance.enqueue(object)
+
+    print(object, file=sys.stdout)
 
 
 def remove(instance):
