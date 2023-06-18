a7b667645e83e78c584fc32b93c1b3b131eea968
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-06-18 17:29:42 -0300

    feat: req 7 done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,31 @@
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+from ting_file_management.file_process import process
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    check_word = txt_importer(word)
+    if not check_word:
+        return []
+    result = {
+        'palavra': word,
+        'arquivo': instance.queue[0]['nome_do_arquivo'],
+        'ocorrencias': [],
+    }
+    for i, line in enumerate(instance.queue[0]['linhas_do_arquivo'],
+                             start=1):
+        if word in line.lower():
+            result['ocorrencias'].append({'linha': i})
+    if len(result['ocorrencias']) == 0:
+        return []
+    print(result)
+    return [result]
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementaçã"""
+
+
+project = Queue()
+process("statics/nome_pedro.txt", project)
+exists_word("pedro", project)
